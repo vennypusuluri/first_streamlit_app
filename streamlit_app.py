72733c2dd0b32c400020aaa2dd4106949b275c2e
vennypusuluri <132473972+vennypusuluri@users.noreply.github.com>
2023-05-03 14:06:38 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 streamlit.title(' My Parents New Healthy Diner')
 streamlit.header(' Breakfast Menu')
 streamlit.text(' 🥣 Omega 3 and BlueBerry Oatmeal')
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.text(' 🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text(' 🐔 Hard-Boiled Free-range Egg')
 streamlit.text(' 🥑 Avocado Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
