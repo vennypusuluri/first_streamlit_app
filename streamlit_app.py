d6cc5f2a657f1354d312a3989300f70837babf4d
vennypusuluri <132473972+vennypusuluri@users.noreply.github.com>
2023-05-03 13:56:58 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title(' My Parents New Healthy Diner')
 streamlit.header(' Breakfast Menu')
-stremalit.text('Omega 3 and BlueBerry Oatmeal')
-stremalit.text('Kale, Spinach & Rocket Smoothie')
-stremalit.text('Hard-Boiled Free-range Egg')
+streamlit.text('Omega 3 and BlueBerry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-range Egg')
